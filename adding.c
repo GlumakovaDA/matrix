b2648835bcd7c70963da7b9a81d949bdc89d60e8
GLumakovaDA <darya.glumakova@yandex.ru>
2024-05-25 01:23:40 +0300

    matrix

---
+++ adding.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include "matrix.h"
+#include "matrix_actions.h"
+
+matrix* adding(matrix* a, matrix* b)
+{
+    if(!a||!b)
+    {
+        return NULL;
+    }
+
+    size_t m = size_m(a);
+    size_t n = size_n(a);
+
+    if(m!= size_m(b) || n!= size_n(b))
+    {
+        return NULL;
+    }
+
+    for(size_t i = 0; i < m; i++)
+    {
+        for(size_t j = 0; j < n; j++)
+        {
+            element_t sum = in_index(a, i, j) + in_index(b, i, j);
+            change_elem(a, i, j, sum);
+        }
+    }
+    return a;
+}
+
+
